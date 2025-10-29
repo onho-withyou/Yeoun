553c9cb36fd67009ca4eecfb026123fcdafe2425
onho-withyou <wndud600@naver.com>
2025-10-29 09:01:01 +0900

    test push

---
+++ src/main/java/com/yeoun/YeounApplication.java
@@ -0,0 +1,13 @@
+package com.yeoun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeounApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeounApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yeoun/YeounApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeoun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeounApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
