e41adff00e5e07434e348aa44a935354d4e6bfe5
SPY <spycode92@gmail.com>
2025-11-02 17:17:53 +0900

    본문 예시 추가

---
+++ src/main/java/com/yeoun/main/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.yeoun.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	@GetMapping("")
+	public String Main() {
+		return "main";
+	}
+	
+}
