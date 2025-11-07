eed51fcb8b0ca266d7b48e4a84d2550161954632
서지원 <ziwonee24@gmail.com>
2025-11-07 17:35:43 +0900

    패스워드 인코더로 사용할 빈 등록

---
+++ src/main/java/com/yeoun/auth/security/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.yeoun.auth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity	// 스프링 시큐리티 기능 설정 클래스로 지정
+public class WebSecurityConfig {
+
+	// 패스워드 인코더로 사용할 빈 등록
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
