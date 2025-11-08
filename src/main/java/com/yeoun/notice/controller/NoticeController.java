4706ef5cba0039f2485dc6e4a3e6dfc43cefc53c
신필용 <spycode92@gmail.com>
2025-11-08 19:40:32 +0900

    공지사항 목록 불러오기 완료

--- src/main/java/com/yeoun/notice/controller/NoticeController.java
+++ src/main/java/com/yeoun/notice/controller/NoticeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yeoun.notice.dto.NoticeDTO;
 import com.yeoun.notice.service.NoticeService;
@@ -20,9 +21,19 @@ public class NoticeController {
 	private final NoticeService noticeService;
 	
 	@GetMapping("")
-	public String notice(Model model) {
+	public String notice(Model model,
+			@RequestParam(defaultValue = "0", name = "page")int page,
+		    @RequestParam(defaultValue = "10", name = "size")int size) {
 		
-		List<NoticeDTO> noticeList = noticeService.getNotice();
+		List<NoticeDTO> noticeDTOList = noticeService.getNotice(page, size);
+		
+		System.out.println("11111111111" + noticeDTOList);
+		
+		for(NoticeDTO notice : noticeDTOList) {
+			System.out.println(notice.getNoticeId());
+		}
+		
+		model.addAttribute("noticeDTOList", noticeDTOList);
 		
 		return "/notice/notice";
 	}
--- src/main/java/com/yeoun/notice/service/NoticeService.java
+++ src/main/java/com/yeoun/notice/service/NoticeService.java
@@ -3,6 +3,9 @@ package com.yeoun.notice.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.yeoun.notice.dto.NoticeDTO;
@@ -16,9 +19,13 @@ import lombok.RequiredArgsConstructor;
 public class NoticeService {
 	private final NoticeRepository noticeRepository;
 	
-	public List<NoticeDTO> getNotice() {
+	public List<NoticeDTO> getNotice(int page, int size) {
 		
-		List<Notice> noticeList = noticeRepository.findAll();
+		Page<Notice> noticePage = noticeRepository.findAll(
+				PageRequest.of(page, size, 
+						Sort.by(Sort.Order.desc("noticeYN"), Sort.Order.desc("updatedDate"))));
+		System.out.println(">>>>>>>>>" + noticePage.getPageable());
+		List<Notice> noticeList = noticePage.getContent();
 		
 		return noticeList.stream()
 				.map(notice -> NoticeDTO.fromEntity(notice))
