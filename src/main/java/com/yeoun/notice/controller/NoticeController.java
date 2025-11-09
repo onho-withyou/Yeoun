ee5dd3a6edcd752bda7cda09f9b011b04b0a1335
신필용 <spycode92@gmail.com>
2025-11-09 22:03:30 +0900

    공지사항 페이지, 정렬 기능완료

--- src/main/java/com/yeoun/notice/controller/NoticeController.java
+++ src/main/java/com/yeoun/notice/controller/NoticeController.java
@@ -1,18 +1,28 @@
 package com.yeoun.notice.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yeoun.notice.dto.NoticeDTO;
+import com.yeoun.notice.entity.Notice;
 import com.yeoun.notice.service.NoticeService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,32 +35,73 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class NoticeController {
 	private final NoticeService noticeService;
 	
+	// 공지사항 목록 조회
 	@GetMapping("")
 	public String notices(Model model,
 			@RequestParam(defaultValue = "0", name = "page")int page,
-		    @RequestParam(defaultValue = "10", name = "size")int size) {
+		    @RequestParam(defaultValue = "2", name = "size")int size,
+		    @RequestParam(defaultValue = "", name = "searchKeyword")String searchKeyword,
+		    @RequestParam(defaultValue = "updatedDate", name = "orderKey")String orderKey,
+		    @RequestParam(defaultValue = "", name = "orderMethod")String orderMethod) {
 		
-		List<NoticeDTO> noticeDTOList = noticeService.getNotice(page, size);
+		Page<NoticeDTO> noticePage = noticeService.getNotice(page, size, searchKeyword, orderKey, orderMethod);
 		
-//		System.out.println("11111111111" + noticeDTOList);
-		
-		for(NoticeDTO notice : noticeDTOList) {
-			System.out.println(notice.getNoticeId());
+	    model.addAttribute("noticeDTOList", noticePage.getContent());
+	    model.addAttribute("currentPage", noticePage.getNumber()); // 현재 페이지
+	    model.addAttribute("totalPages", noticePage.getTotalPages()); // 
+	    model.addAttribute("searchKeyword", searchKeyword);
+	    model.addAttribute("orderKey", orderKey);
+	    model.addAttribute("orderMethod", orderMethod);
+	    
+		return "/notice/notice";
+	}
+	
+	//공지사항 등록 로직
+	@PostMapping("")
+	public ResponseEntity<Map<String, String>> notices(@ModelAttribute("noticeDTO") @Valid NoticeDTO noticeDTO, BindingResult bindingResult) {
+		Map<String, String> msg = new HashMap<>();
+		if(bindingResult.hasErrors()) {
+			msg.put("msg", "공지사항 등록에 실패했습니다2222");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
 		}
 		
-		model.addAttribute("noticeDTOList", noticeDTOList);
+		try {
+			// 공지 등록 수행
+			noticeService.createNotice(noticeDTO);
+			msg.put("msg", "공지사항이 등록되었습니다.");
+			return ResponseEntity.ok(msg);
 		
-		return "/notice/notice";
+		} catch (Exception e) {
+			System.out.println(e);
+			msg.put("msg", "공지사항 등록에 실패했습니다 :" + e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
+		}
 	}
 	
-	@PostMapping("")
-	public String notices(@ModelAttribute NoticeDTO noticeDTO) {
+	//공지사항 수정 로직
+	@PatchMapping("/{noticeId}")
+	public ResponseEntity<Map<String, String>> notices( @PathVariable("noticeId")Long noticeId,
+			@ModelAttribute("noticeDTO") @Valid NoticeDTO noticeDTO, BindingResult bindingResult) {
+		Map<String, String> msg = new HashMap<>();
 		
+		if(bindingResult.hasErrors()) {
+			msg.put("msg", "공지사항 수정에 실패했습니다");
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
+		}
 		
-		
-		return "/notice/notice";
+		System.out.println(noticeDTO + "이거왜 ?@@@@@@@@@@@@@@@@@");
+		try {
+			// 공지 등록 수행
+			noticeService.modifyNotice(noticeDTO);
+			msg.put("msg", "공지사항이 수정되었습니다.");
+			return ResponseEntity.ok(msg);
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			msg.put("msg", "공지사항 수정에 실패했습니다 :" + e.getMessage());
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(msg);
+		}
 	}
-	
 }
 
 
--- src/main/java/com/yeoun/notice/repository/NoticeRepository.java
+++ src/main/java/com/yeoun/notice/repository/NoticeRepository.java
@@ -1,5 +1,7 @@
 package com.yeoun.notice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.yeoun.notice.entity.Notice;
 
 @Repository
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
+	Page<Notice> findByNoticeTitleContainingOrNoticeContentContaining(String title, String content, Pageable pageable);
 
 }
