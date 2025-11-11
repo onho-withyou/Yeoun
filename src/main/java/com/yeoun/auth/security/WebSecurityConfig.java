1847b666cba8c03542d084d974b1eecfa87bf750
서지원 <ziwonee24@gmail.com>
2025-11-11 12:10:41 +0900

    자동 로그인 기능 추가

--- src/main/java/com/yeoun/auth/security/WebSecurityConfig.java
+++ src/main/java/com/yeoun/auth/security/WebSecurityConfig.java
@@ -30,39 +30,44 @@ public class WebSecurityConfig {
 				// --------- 요청에 대한 접근 허용 여부 등의 요청 경로에 대한 권한 설정 -------
 				.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
 						
-						// 1) 스프링이 인식하는 "공통 정적 리소스" 허용
-						.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-						
-						// 2) 프로젝트에서 실제 쓰는 정적 경로들 "명시" 허용
-						.requestMatchers("/assets/**", "/css/**", "/custom_bg/**", "/icon/**", "/js/**").permitAll()
-						
-						// 3) 로그인 페이지 자체는 누구나 접근 가능
-						.requestMatchers("/", "/main", "/login").permitAll()
-						
-						
-						.anyRequest().authenticated() // 그 외 모든 요청은 인증된 사용자만 접근 가능
+					// 1) 스프링이 인식하는 "공통 정적 리소스" 허용
+					.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+					
+					// 2) 프로젝트에서 실제 쓰는 정적 경로들 "명시" 허용
+					.requestMatchers("/assets/**", "/css/**", "/custom_bg/**", "/icon/**", "/js/**").permitAll()
+					
+					// 3) 로그인 페이지 자체는 누구나 접근 가능
+					.requestMatchers("/", "/main", "/login").permitAll()
+					
+					
+					.anyRequest().authenticated() // 그 외 모든 요청은 인증된 사용자만 접근 가능
 //						.anyRequest().denyAll() // 그 외의 모든 요청은 거부
-					)
-					// 
-					.formLogin(login -> login
-						.loginPage("/login") // 로그인 폼 요청에 사용할 URL 지정(컨트롤러에서 매핑 처리)
-						.loginProcessingUrl("/login") // 로그인 폼에서 제출된 데이터 처리용 요청 주소(자동으로 POST 방식으로 처리됨)
-						// => 여기서 지정한 경로는 컨트롤러에서 별도의 매핑 불필요(단, 로그인 처리 과정에서 부가적인 기능 추가할 경우 매핑 필요)
-						// => 이 과정에서 UserDetailsService(또는 구현체) 객체의 loadByUsername() 메서드가 자동으로 호출됨
-						.usernameParameter("empId") // 로그인 과정에서 사용할 사용자명(username)을 사원번호(empId)로 지정(기본값 : username)
-						.passwordParameter("empPwd") // 로그인 과정에서 사용할 패스워드 지정(기본값 : password)
+				)
+				// 
+				.formLogin(login -> login
+					.loginPage("/login") // 로그인 폼 요청에 사용할 URL 지정(컨트롤러에서 매핑 처리)
+					.loginProcessingUrl("/login") // 로그인 폼에서 제출된 데이터 처리용 요청 주소(자동으로 POST 방식으로 처리됨)
+					// => 여기서 지정한 경로는 컨트롤러에서 별도의 매핑 불필요(단, 로그인 처리 과정에서 부가적인 기능 추가할 경우 매핑 필요)
+					// => 이 과정에서 UserDetailsService(또는 구현체) 객체의 loadByUsername() 메서드가 자동으로 호출됨
+					.usernameParameter("empId") // 로그인 과정에서 사용할 사용자명(username)을 사원번호(empId)로 지정(기본값 : username)
+					.passwordParameter("empPwd") // 로그인 과정에서 사용할 패스워드 지정(기본값 : password)
 //						.defaultSuccessUrl("/main", true) // 로그인 성공 시 항상 리디렉션 할 기본 URL 설정
-						.successHandler(new CustomAuthenticationSuccessHandler())  // 로그인 성공 시 별도의 추가 작업을 처리할 핸들러 지정
-						.permitAll() // 로그인 경로 관련 요청 주소를 모두 허용
-					) 
-					// ---------- 로그아웃 처리 설정 ---------
-					.logout(logout -> logout
-						.logoutUrl("/logout") // 로그아웃 요청 URL 지정(주의! POST 방식 요청으로 취급함)
-						.logoutSuccessUrl("/login") // 로그아웃 성공 후 리디렉션 할 URL 지정
-						.permitAll()
-					)
-					//
-					.build();
+					.successHandler(new CustomAuthenticationSuccessHandler())  // 로그인 성공 시 별도의 추가 작업을 처리할 핸들러 지정
+					.permitAll() // 로그인 경로 관련 요청 주소를 모두 허용
+				) 
+				// ---------- 로그아웃 처리 설정 ---------
+				.logout(logout -> logout
+					.logoutUrl("/logout") // 로그아웃 요청 URL 지정(주의! POST 방식 요청으로 취급함)
+					.logoutSuccessUrl("/login") // 로그아웃 성공 후 리디렉션 할 URL 지정
+					.permitAll()
+				)
+				// ---------- 자동 로그인 처리 설정 ----------
+				.rememberMe(rememberMeCustormizer -> rememberMeCustormizer
+						.rememberMeParameter("remember-me") // 자동 로그인 수행을 위한 체크박스 파라미터명 지정(체크 여부 자동으로 판별)
+						.key("my-fixed-secret-key") // 서버 재시작해도 이전 로그인에서 사용했던 키 동일하게 사용
+						.tokenValiditySeconds(60 * 60 * 24) // 자동 로그인 토큰 유효기간 설정(기본값 14일 => 1일로 변경)
+				)
+				.build();
     }
 
 	// 패스워드 인코더로 사용할 빈 등록
