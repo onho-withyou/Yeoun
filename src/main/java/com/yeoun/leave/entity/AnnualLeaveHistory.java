674181258c5b01b64b318f4f448091762d618aca
이정민 <yoyo3302@gmail.com>
2025-11-15 20:22:50 +0900

    연차 현황 빌더 패턴 추가

--- src/main/java/com/yeoun/leave/entity/AnnualLeaveHistory.java
+++ src/main/java/com/yeoun/leave/entity/AnnualLeaveHistory.java
@@ -16,6 +16,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -66,4 +67,18 @@ public class AnnualLeaveHistory {
 	private String apprStatus; // 결재 상태 (승인/반려) / (APPROVED / REJECTED)
 	
 	private Long paymentId; // 결재문서 Id
+	
+	@Builder
+	public AnnualLeaveHistory(Emp emp, AnnualLeave annualLeave, String leaveType, LocalDate startDate,
+			LocalDate endDate, int usedDays, String reason, Long paymentId) {
+		this.emp = emp;
+		this.annualLeave = annualLeave;
+		this.leaveType = leaveType;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.usedDays = usedDays;
+		this.reason = reason;
+		this.apprStatus = "REJECTED";
+		this.paymentId = paymentId;
+	}
 }
