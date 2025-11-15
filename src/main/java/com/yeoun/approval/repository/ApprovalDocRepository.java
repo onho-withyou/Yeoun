e7a0c1012bccaebda42228d41e1c043aae737497
박다정 <ekwjd0430@naver.com>
2025-11-16 06:36:12 +0900

    mod.결재 - 전체결재 grid 띄우기

--- src/main/java/com/yeoun/approval/repository/ApprovalDocRepository.java
+++ src/main/java/com/yeoun/approval/repository/ApprovalDocRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.yeoun.approval.entity.ApprovalDoc;
+import com.yeoun.approval.entity.ApprovalList;
 import com.yeoun.emp.entity.Dept;
 import com.yeoun.emp.entity.Emp;
 
@@ -28,34 +29,36 @@ public interface ApprovalDocRepository extends JpaRepository<ApprovalDoc, Long> 
 
 	// 그리드 - 1.결재사항 - 진행해야할 결재만 - 결재권한자만 볼수있음
 	// 그리드 - 2.전체결재- 나와관련된 모든 결재문서
-	@Query(value="""
-			SELECT adr.approvalId
-       				,adr.approvalTitle
-       				,adr.empId
-       				,e.empName
-       				,e.deptId
-       				,d.deptName
-       				,adr.approver
-       				,p.posCode
-       				,p.posName
-       				,adr.createdDate
-       				,adr.finishDate
-       				,adr.docStatus
-			FROM Emp e, Dept d, Position p,
-				( SELECT DISTINCT
-    					ad.approvalId
-    					,ad.approvalTitle
-    					,ad.empId
+	@Query(value = """
+				SELECT  rownum AS rnum
+						,adr.approval_id AS approvalId
+       					,adr.approval_title AS approvalTitle
+       					,adr.emp_id AS empId
+       					,e.emp_name	AS empName
+       					,e.dept_id	AS deptId
+       					,d.dept_name AS deptName
+       					,adr.approver AS approver
+       					,p.pos_code  AS posCode
+       					,p.pos_name  AS posName
+       					,adr.created_date AS createdDate
+       					,adr.finish_date AS finishDate
+       					,adr.doc_status AS docStatus
+				FROM emp e, dept d,position p,
+					( SELECT distinct 
+    					ad.approval_id
+    					,ad.approval_title
+    					,ad.emp_id
     					,ad.approver
-    					,ad.createdDate
-    					,ad.finishDate
-    					,ad.docStatus
-				FROM ApprovalDoc ad, Approver ar
-				WHERE (ad.approver = ar.empId AND ar.viewing = 'y') OR ad.empId = :empId) adr
-			WHERE e.empId = adr.empId 
-			AND e.deptId = d.deptId
-			AND e.posCode = p.posCode
-			""", nativeQuery = true)	
+    					,ad.created_date
+    					,ad.finish_date
+    					,ad.doc_status
+					FROM approval_doc ad,approver ar
+					WHERE (ad.approver = ar.emp_id AND ar.viewing = 'y') OR ad.emp_id = :empId ) adr
+				WHERE e.emp_id = adr.emp_id 
+				AND e.dept_id = d.dept_id
+				AND e.pos_code = p.pos_code
+				
+				""", nativeQuery = true)	
 	List<Object[]> findAllApprovalDocs(@Param("empId") String empId);
 
 	// 그리드 - 3.내결재목록 - 내가 올린결재목록
@@ -63,5 +66,6 @@ public interface ApprovalDocRepository extends JpaRepository<ApprovalDoc, Long> 
 	// 그리드 - 5.결재완료 - 결재권한자가 결재를 완료하면 볼수 있음(1차,2차,3차 모든결재 완료시)
 
 			
+
 
 }
