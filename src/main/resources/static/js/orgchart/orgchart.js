e1f0489a74dee46eb420e8ce123765d18b015c47
서지원 <ziwonee24@gmail.com>
2025-11-18 09:05:08 +0900

    js 분리

--- src/main/resources/static/js/orgchart/orgchart.js
+++ src/main/resources/static/js/orgchart/orgchart.js
@@ -1,1 +1,24 @@
 // orgchart.js
+
+$(function () {
+  const orgData = {
+    name: '대표이사',
+    title: 'CEO',
+    children: [
+      { name: '김개발', title: '개발팀장' },
+      {
+        name: '박인사', title: '인사팀장',
+        children: [
+          { name: '이사원', title: '인사담당' },
+          { name: '최사원', title: '채용담당' }
+        ]
+      },
+      { name: '정영업', title: '영업팀장' }
+    ]
+  };
+
+  $('#chart-container').orgchart({
+    data: orgData,
+    nodeContent: 'title'
+  });
+});
