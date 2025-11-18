b0471ae48646b89c51b9987f533278d092681c29
서지원 <ziwonee24@gmail.com>
2025-11-18 09:48:57 +0900

    테스트 CSS 추후 색 변경

--- src/main/resources/static/js/orgchart/orgchart.js
+++ src/main/resources/static/js/orgchart/orgchart.js
@@ -2,20 +2,39 @@
 
 $(function () {
   const orgData = {
-    name: '대표이사',
-    title: 'CEO',
-    children: [
-      { name: '김개발', title: '개발팀장' },
-      {
-        name: '박인사', title: '인사팀장',
-        children: [
-          { name: '이사원', title: '인사담당' },
-          { name: '최사원', title: '채용담당' }
-        ]
-      },
-      { name: '정영업', title: '영업팀장' }
-    ]
-  };
+	name: 'CEO',
+	title: '대표이사',
+	className: 'dept-ceo',
+	children: [
+	    {
+	      name: '개발팀장',
+	      title: '개발팀장',
+	      className: 'dept-dev'
+	    },
+	    {
+	      name: '인사팀장',
+	      title: '인사팀장',
+	      className: 'dept-hr',
+	      children: [
+	        {
+	          name: '인사담당',
+	          title: '인사담당',
+	          className: 'dept-hr'
+	        },
+	        {
+	          name: '채용담당',
+	          title: '채용담당',
+	          className: 'dept-hr'
+	        }
+	      ]
+	    },
+	    {
+	      name: '영업팀장',
+	      title: '영업팀장',
+	      className: 'dept-sales'
+	    }
+	  ]
+	};
 
   $('#chart-container').orgchart({
     data: orgData,
