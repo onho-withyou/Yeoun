67ad3ef3b51a89c87e3a54754cd4a8e110547b24
서지원 <ziwonee24@gmail.com>
2025-11-19 12:11:22 +0900

    재직자만 로그인 가능, 아이디비번 틀렸을 경우 msg

--- src/main/java/com/yeoun/auth/security/WebSecurityConfig.java
+++ src/main/java/com/yeoun/auth/security/WebSecurityConfig.java
@@ -41,35 +41,31 @@ public class WebSecurityConfig {
 					// 공개 페이지
 					.requestMatchers("/", "/main", "/login", "/logout").permitAll()
 				
-//					// HR/인사 화면: URL은 “로그인만” 통과 → 세부 권한은 메소드 보안에서
-//				    .requestMatchers("/emp/**").authenticated()
-	                
 	            // =========================================================================
                 // 그 외 모든 요청은 인증 필요 
 					.anyRequest().authenticated() 
 				)
 				// ---------- 로그인 처리 설정 ---------
 				.formLogin(login -> login
-					.loginPage("/login") // 로그인 폼 요청에 사용할 URL 지정(컨트롤러에서 매핑 처리)
-					.loginProcessingUrl("/login") // 로그인 폼에서 제출된 데이터 처리용 요청 주소(자동으로 POST 방식으로 처리됨)
-					// => 여기서 지정한 경로는 컨트롤러에서 별도의 매핑 불필요(단, 로그인 처리 과정에서 부가적인 기능 추가할 경우 매핑 필요)
-					// => 이 과정에서 UserDetailsService(또는 구현체) 객체의 loadByUsername() 메서드가 자동으로 호출됨
-					.usernameParameter("empId") // 로그인 과정에서 사용할 사용자명(username)을 사원번호(empId)로 지정(기본값 : username)
-					.passwordParameter("empPwd") // 로그인 과정에서 사용할 패스워드 지정(기본값 : password)
+					.loginPage("/login") 			// 로그인 폼 요청에 사용할 URL 지정(컨트롤러에서 매핑 처리)
+					.loginProcessingUrl("/login") 	// 로그인 폼에서 제출된 데이터 처리용 요청 주소(자동으로 POST 방식으로 처리됨)
+					.usernameParameter("empId") 	// 로그인 과정에서 사용할 사용자명(username)을 사원번호(empId)로 지정(기본값 : username)
+					.passwordParameter("empPwd") 	// 로그인 과정에서 사용할 패스워드 지정(기본값 : password)
 					.successHandler(new CustomAuthenticationSuccessHandler())  // 로그인 성공 시 별도의 추가 작업을 처리할 핸들러 지정
-					.permitAll() // 로그인 경로 관련 요청 주소를 모두 허용
+					.failureUrl("/login?error")		// 로그인 실패 시 리다이렉트
+					.permitAll() 					// 로그인 경로 관련 요청 주소를 모두 허용
 				) 
 				// ---------- 로그아웃 처리 설정 ---------
 				.logout(logout -> logout
-					.logoutUrl("/logout") // 로그아웃 요청 URL 지정(주의! POST 방식 요청으로 취급함)
-					.logoutSuccessUrl("/login?logout") // 로그아웃 성공 후 리디렉션 할 URL 지정
+					.logoutUrl("/logout") 				// 로그아웃 요청 URL 지정(POST 방식 요청으로 취급함)
+					.logoutSuccessUrl("/login?logout") 	// 로그아웃 성공 후 리디렉션 할 URL 지정
 					.permitAll()
 				)
 				// ---------- 자동 로그인 처리 설정 ----------
 				.rememberMe(rememberMeCustormizer -> rememberMeCustormizer
-						.rememberMeParameter("remember-me") // 자동 로그인 수행을 위한 체크박스 파라미터명 지정(체크 여부 자동으로 판별)
-						.key("my-fixed-secret-key") // 서버 재시작해도 이전 로그인에서 사용했던 키 동일하게 사용
-						.tokenValiditySeconds(60 * 60 * 24 * 30) // 자동 로그인 토큰 유효기간 설정(30일)
+						.rememberMeParameter("remember-me") 		// 자동 로그인 수행을 위한 체크박스 파라미터명 지정(체크 여부 자동으로 판별)
+						.key("my-fixed-secret-key") 				// 서버 재시작해도 이전 로그인에서 사용했던 키 동일하게 사용
+						.tokenValiditySeconds(60 * 60 * 24 * 30) 	// 자동 로그인 토큰 유효기간 설정(30일)
 				)
 				.build();
     }
