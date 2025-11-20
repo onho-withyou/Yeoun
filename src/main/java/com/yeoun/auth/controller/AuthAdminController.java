4177e890f11ccea3019d6646110fe025c622a926
서지원 <ziwonee24@gmail.com>
2025-11-20 11:44:21 +0900

    사원및역할 목록, 뷰 구현

---
+++ src/main/java/com/yeoun/auth/controller/AuthAdminController.java
@@ -0,0 +1,84 @@
+package com.yeoun.auth.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yeoun.auth.service.AuthAdminService;
+import com.yeoun.emp.dto.EmpListDTO;
+import com.yeoun.emp.repository.DeptRepository;
+
+import lombok.RequiredArgsConstructor;
+
+// 관리자 - 접근권한 부여
+@Controller
+@RequestMapping("/auth/manage")
+@RequiredArgsConstructor
+public class AuthAdminController {
+	
+	private final AuthAdminService authAdminService;
+    private final DeptRepository deptRepository;
+	
+	// 권한관리 메인 화면
+	@GetMapping("")
+	public String managePage(Model model) {
+		
+		model.addAttribute("deptList", deptRepository.findActive());
+		model.addAttribute("roleList", authAdminService.getAllRoles());
+		
+		return "auth/manage";
+	}
+	
+	// 사원 목록 데이터 API
+	@GetMapping("/data")
+	@ResponseBody
+	public Map<String, Object> getEmpData(@RequestParam(required = false, name = "keyword") String keyword,
+										  @RequestParam(required = false, name = "deptId") String deptId) {
+		
+		List<EmpListDTO> list = authAdminService.getEmpListForAuth(deptId, keyword);
+		
+		Map<String, Object> pagination = new HashMap<>();
+        pagination.put("page", 1);
+        pagination.put("totalCount", list.size());
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("contents", list);
+        data.put("pagination", pagination);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("result", true);
+        result.put("data", data);
+		
+		return result;
+	}
+	
+	
+
+} // AuthAdminController 끝
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/yeoun/auth/service/AuthAdminService.java
@@ -0,0 +1,53 @@
+package com.yeoun.auth.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yeoun.auth.dto.RoleDTO;
+import com.yeoun.auth.repository.RoleRepository;
+import com.yeoun.emp.dto.EmpListDTO;
+import com.yeoun.emp.repository.EmpRepository;
+import com.yeoun.emp.repository.EmpRoleRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AuthAdminService {
+	
+	private final EmpRepository empRepository;
+	private final RoleRepository roleRepository;
+	private final EmpRoleRepository empRoleRepository;
+	
+	// 1. 사원 목록 조회
+	@Transactional(readOnly = true)
+	public List<EmpListDTO> getEmpListForAuth(String deptId, String Keyword) {
+		return empRepository.searchActiveEmpList(deptId, null, Keyword);
+	}
+	
+	// 2. 역할 조회
+	@Transactional(readOnly = true)
+	public List<RoleDTO> getAllRoles() {
+		
+		return roleRepository.findActive().stream()
+				.map(r -> new RoleDTO(r.getRoleCode(), r.getRoleName()))
+				.toList();
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} // AuthAdminService 끝
