9d806635cc84242264e03b497dadfa308787bd4f
서지원 <ziwonee24@gmail.com>
2025-11-20 16:36:54 +0900

    비밀번호 및 내정보 보기

--- src/main/java/com/yeoun/auth/controller/MyPageController.java
+++ src/main/java/com/yeoun/auth/controller/MyPageController.java
@@ -2,7 +2,6 @@ package com.yeoun.auth.controller;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
