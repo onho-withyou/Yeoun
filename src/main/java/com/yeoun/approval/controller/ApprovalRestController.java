3a4f17c2d86f2d8b2fe765a404425d825e5a4ea9
신필용 <spycode92@gmail.com>
2025-11-21 23:17:28 +0900

    머지후 오류 수정

--- src/main/java/com/yeoun/approval/controller/ApprovalRestController.java
+++ src/main/java/com/yeoun/approval/controller/ApprovalRestController.java
@@ -12,13 +12,19 @@ import com.yeoun.approval.service.ApprovalDocService;
 import com.yeoun.auth.dto.LoginDTO;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
