164cd81b80aedde4a7240f49bc1df10203b98f1d
서지원 <ziwonee24@gmail.com>
2025-11-23 06:36:18 +0900

    비밀번호 규칙 적용

--- src/main/java/com/yeoun/auth/dto/PasswordChangeDTO.java
+++ src/main/java/com/yeoun/auth/dto/PasswordChangeDTO.java
@@ -1,7 +1,7 @@
 package com.yeoun.auth.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +13,10 @@ public class PasswordChangeDTO {
     private String currentPassword;
 
     @NotBlank(message = "새 비밀번호를 입력하세요.")
-    @Size(min = 8, message = "새 비밀번호는 8자 이상이어야 합니다.")
+    @Pattern(
+    	    regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).{8,}$",
+    	    message = "비밀번호는 대/소문자, 숫자, 특수문자를 포함하여 8자 이상이어야 합니다."
+    	)
     private String newPassword;
 
     @NotBlank(message = "새 비밀번호 확인을 입력하세요.")
