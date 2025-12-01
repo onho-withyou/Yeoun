6803eec779c2095feb16a6683ed3b2df3909e23d
박다정 <ekwjd0430@naver.com>
2025-12-01 21:33:18 +0900

    length추가

--- src/main/java/com/yeoun/masterData/dto/MaterialMstDTO.java
+++ src/main/java/com/yeoun/masterData/dto/MaterialMstDTO.java
@@ -1,14 +1,9 @@
-package com.yeoun.masterData.entity;
+package com.yeoun.masterData.dto;
 
 import java.time.LocalDate;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
