001fd689fa3f06360a21dcc433b71f3cef8103b6
이정민 <yoyo3302@gmail.com>
2025-12-01 22:50:41 +0900

    발주 컨트롤러 추가

---
+++ src/main/java/com/yeoun/inventory/controller/PurchaseController.java
@@ -0,0 +1,15 @@
+package com.yeoun.inventory.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/purchase")
+public class PurchaseController {
+	
+	@GetMapping("/purchaseOrder")
+	public String purchaseOrderList() {
+		return "/purchase/purchase_list";
+	}
+}
