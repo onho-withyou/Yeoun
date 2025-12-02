f22e9467d4b3ff176c92a6e611f058aa76d35af4
이정민 <yoyo3302@gmail.com>
2025-12-03 00:23:02 +0900

    공급업체와 공급물품 정보 가져오기 위한 파일 생성

---
+++ src/main/java/com/yeoun/inventory/mapper/PurchaseMapper.java
@@ -0,0 +1,14 @@
+package com.yeoun.inventory.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.yeoun.inventory.dto.SupplierDTO;
+
+@Mapper
+public interface PurchaseMapper {
+
+	// 공급업체 조회
+	List<SupplierDTO> findAllSuppliers();
+}
