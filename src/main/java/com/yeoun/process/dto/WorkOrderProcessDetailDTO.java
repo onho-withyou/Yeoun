920da856c6899bc34c31ee935ba0d4123591ecd3
ziwoneee <ziwonee24@gmail.com>
2025-12-03 16:19:29 +0900

    공정현황 상세 모달 작업중

---
+++ src/main/java/com/yeoun/process/dto/WorkOrderProcessDetailDTO.java
@@ -0,0 +1,13 @@
+package com.yeoun.process.dto;
+
+import java.util.List;
+
+public class WorkOrderProcessDetailDTO {
+	
+	// 모달 상단
+	private WorkOrderProcessDTO wop;
+	
+	// 공정 단계 리스트
+	private List<WorkOrderProcessStepDTO> steps;
+
+}
