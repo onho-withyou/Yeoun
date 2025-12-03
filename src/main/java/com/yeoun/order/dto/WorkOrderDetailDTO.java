4bef541dd33e10b544497c5630418607a383e07a
조주영 <wndud600@naver.com>
2025-12-03 17:38:52 +0900

    엔티티 정리 및 목록 조회

---
+++ src/main/java/com/yeoun/order/dto/WorkOrderDetailDTO.java
@@ -0,0 +1,14 @@
+package com.yeoun.order.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class WorkOrderDetailDTO {
+
+}
