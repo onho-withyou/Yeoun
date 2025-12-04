00b5835665bee5d9e20fbff2fee43fbc0b637c30
조주영 <wndud600@naver.com>
2025-12-05 03:27:55 +0900

    작업지시 조회 상세구현중

--- src/main/java/com/yeoun/order/mapper/OrderMapper.java
+++ src/main/java/com/yeoun/order/mapper/OrderMapper.java
@@ -2,6 +2,7 @@ package com.yeoun.order.mapper;
 
 import java.util.List;
 
+import com.yeoun.order.dto.WorkOrderSearchDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,6 @@ import com.yeoun.order.dto.WorkOrderListDTO;
 public interface OrderMapper {
 	
 	// 작업지시 리스트 모두 불러오기
-	List<WorkOrderListDTO> selectOrderList (WorkOrderListDTO dto);
+	List<WorkOrderListDTO> selectOrderList (WorkOrderSearchDTO dto);
 
 }
