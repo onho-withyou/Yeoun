2ed614a8b9b669810a72c1adeb923656e71be6a0
parkdajung <ekwjd0430@naver.com>
2025-12-05 16:54:01 +0900

    mod. pk

--- src/main/java/com/yeoun/masterData/repository/BomMstRepository.java
+++ src/main/java/com/yeoun/masterData/repository/BomMstRepository.java
@@ -3,8 +3,9 @@ package com.yeoun.masterData.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yeoun.masterData.entity.BomMst;
+import com.yeoun.masterData.entity.BomMstId;
 import com.yeoun.masterData.entity.MaterialMst;
 
-public interface BomMstRepository extends JpaRepository<BomMst, String>{
+public interface BomMstRepository extends JpaRepository<BomMst, BomMstId>{
 
 }
