41b01a2d71cea188314d85c32826abc11058c2de
parkdajung <ekwjd0430@naver.com>
2025-12-05 17:04:53 +0900

    mod. 충돌해결

--- src/main/java/com/yeoun/masterData/repository/BomMstRepository.java
+++ src/main/java/com/yeoun/masterData/repository/BomMstRepository.java
@@ -3,9 +3,6 @@ package com.yeoun.masterData.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.yeoun.masterData.entity.BomMst;
-import com.yeoun.masterData.entity.MaterialMst;
-
-public interface BomMstRepository extends JpaRepository<BomMst, String>{
 import com.yeoun.masterData.entity.BomMstId;
 import com.yeoun.masterData.entity.MaterialMst;
 
