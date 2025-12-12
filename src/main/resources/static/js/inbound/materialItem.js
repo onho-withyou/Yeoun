b537e03572b5fcee7d59abf9c4f07d1fabab218d
이정민 <yoyo3302@gmail.com>
2025-12-12 09:59:23 +0900

    입고 유효성 검사

--- src/main/resources/static/js/inbound/materialItem.js
+++ src/main/resources/static/js/inbound/materialItem.js
@@ -92,6 +92,9 @@ function applyRackSelection(index) {
 
 // 페이지 로딩될 때 select 및 수량 정보 채우기
 document.addEventListener("DOMContentLoaded", async () => {
+	//스피너  off
+	hideSpinner(); 
+	
 	// 창고 데이터 가져오기
 	locationInfo = await getLocationInfo();
 	
@@ -268,6 +271,8 @@ document.getElementById("completeInboundBtn").addEventListener("click", async ()
 		const inboundAmount = Number(row.querySelector(`.inboundAmount[data-index="${index}"]`).value);
 		// 폐기 수량
 		const disposeAmount = Number(row.querySelector(`.disposeAmount[data-index="${index}"]`).textContent);
+		// 입고 요청 수량
+		const requestAmount = Number(row.querySelector(`.requestAmount[data-index="${index}"]`).textContent);
 		// 발주 단위
 		const unit = row.querySelector(`.unit[data-index="${index}"]`).innerText;
 		
@@ -284,8 +289,13 @@ document.getElementById("completeInboundBtn").addEventListener("click", async ()
 		}
 		
 		// 입고 수량 입력 확인
-		if (inboundAmount <= 0) {
+		if (inboundAmount < 0) {
 			alert("입고 수량을 입력해주세요.");
+			return;
+		}
+		
+		if (requestAmount !== (inboundAmount + disposeAmount)) {
+			alert("입고 수량 또는 폐기 수량을 입력해주세요.");
 			return;
 		}
 		
@@ -311,31 +321,42 @@ document.getElementById("completeInboundBtn").addEventListener("click", async ()
 
 	const inboundId = document.querySelector("#inboundId").value;
 	
-	const res = await fetch("/inventory/inbound/mat/complete", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			[csrfHeader]: csrfToken
-		},
-		body: JSON.stringify({
-			inboundId,
-			type: "MAT",
-			items
-		})
-	});
+	// 스피너 시작
+	showSpinner();
 	
-	if (!res.ok) {
-		alert("입고 등록을 실패했습니다.");
-		return;
-	}
-	
-	const data = await res.json();
-	
-	if (data.success) {
-		alert("입고가 완료되었습니다.");
-		setTimeout(() => {
-			window.location.href = "/inventory/inbound";
-		}, 10); 
+	try {
+		const res = await fetch("/inventory/inbound/mat/complete", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				[csrfHeader]: csrfToken
+			},
+			body: JSON.stringify({
+				inboundId,
+				type: "MAT",
+				items
+			})
+		});
+		
+		if (!res.ok) {
+			alert("입고 등록을 실패했습니다.");
+			return;
+		}
+		
+		const data = await res.json();
+		
+		if (data.success) {
+			alert("입고가 완료되었습니다.");
+			setTimeout(() => {
+				window.location.href = "/inventory/inbound";
+			}, 10); 
+		}
+	} catch (error) {
+		console.error(error);
+		alert("요청 처리 중 오류가 발생했습니다." || error.message);
+	} finally {
+		//스피너  off
+		hideSpinner(); 
 	}
 	
 });
