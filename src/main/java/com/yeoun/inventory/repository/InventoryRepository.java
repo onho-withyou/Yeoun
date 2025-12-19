6c9f807dd2d58c750dde7d63db3ea2f2217c4302
이정민 <yoyo3302@gmail.com>
2025-12-19 17:13:58 +0900

    출고 등록 로직 개선

--- src/main/java/com/yeoun/inventory/repository/InventoryRepository.java
+++ src/main/java/com/yeoun/inventory/repository/InventoryRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -115,14 +116,15 @@ public interface InventoryRepository
 	@Query("""
 		    SELECT i
 		    FROM Inventory i
-		    WHERE i.itemId = :itemId
+		    JOIN FETCH i.warehouseLocation
+		    WHERE i.itemId IN :itemIds
 		      AND i.ivStatus <> :status
 		    ORDER BY
-			    i.expirationDate ASC,
-			    i.ibDate ASC,
-			    i.ivAmount DESC
+		        i.itemId ASC,
+		        i.expirationDate ASC,
+		        i.ibDate ASC
 		""")
-	List<Inventory> findByItemIdAndIvStatusNot(@Param("itemId") String itemId, @Param("status") String status);
+	List<Inventory> findByItemIdAndIvStatusNot(@Param("itemIds") Set<String> itemsIds, @Param("status") String status);
 
 	// 재고삭제
 	void delete(Inventory stock);
@@ -149,6 +151,4 @@ public interface InventoryRepository
 
 	// 창고 위치 ID로 재고 존재 확인
 	boolean existsByWarehouseLocation_LocationId(String locationId);
-
-
 }
