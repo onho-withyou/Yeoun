395fe3b3a4e272fe1273ccc7142e6c419f6eecd5
조주영 <wndud600@naver.com>
2025-12-26 17:39:22 +0900

    설비 상태 변경 메서드 추가

--- src/main/java/com/yeoun/equipment/mapper/EquipmentMapper.java
+++ src/main/java/com/yeoun/equipment/mapper/EquipmentMapper.java
@@ -2,6 +2,7 @@ package com.yeoun.equipment.mapper;
 
 import com.yeoun.equipment.dto.EquipDowntimeDTO;
 import com.yeoun.equipment.dto.HistorySearchDTO;
+import com.yeoun.equipment.dto.ProdEquipDTO;
 import com.yeoun.process.dto.WorkOrderProcessDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -19,5 +20,8 @@ public interface EquipmentMapper {
 	
 	// 현재 가동중인!! 설비
 	Integer countRunningEquipments();
+	
+	// 라인과 공정에 해당하는 설비 찾기
+	List<ProdEquipDTO> selectProdEquipByLineAndProcess (String line, String process);
 
 }
